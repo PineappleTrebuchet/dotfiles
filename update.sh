9ab170b6ade370b16674a4439000e90e2eeeebfd
Nirav Eati <niraveati23@gmail.com>
2025-10-28 21:15:51 -0400

    Additions
    Added Thunar and xfce4 to .gitignore, added update script, added GNU Public License, tweaked fuzzel config

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm chrome/* -r
+rm config/* -r
+rm icons/* -r
+rm themes/* -r
+
+cp ~/.waterfox/'pCOlJDgg.Profile 1'/chrome/* chrome/ -r
+cp ~/.config/* config/ -r
+cp ~/.icons/* icons/ -r
+cp ~/.themes/* themes/ -r
